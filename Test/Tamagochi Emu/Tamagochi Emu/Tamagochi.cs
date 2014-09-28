0399f9a8bd0cc31915b38e63cee2054eec5fa5ab
unknown <liambarrell@outlook.com>
2014-09-28 13:56:54 +1000

    Tamagochi class

---
+++ Test/Tamagochi Emu/Tamagochi Emu/Tamagochi.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tamagochi_Emu
+{
+    class Tamagochi
+    {
+        String name;
+        int Health;
+        int Hunger;
+        int Happiness;
+        Double Lifespan;
+        int Love;
+        int Level;
+    }
+}
