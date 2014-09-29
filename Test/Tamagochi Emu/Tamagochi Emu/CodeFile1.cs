1d2ecbcb495f570ad6cecade955bde49f9ba85e5
Josh <legril123@gmail.com>
2014-09-29 20:35:37 +1000

    Added Threading Test Proj, Possibly broke Test

---
+++ Test/Tamagochi Emu/Tamagochi Emu/CodeFile1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+
+class Mayan {
+
+    static string input = "null";
+
+    
+
+    public void getInput() {
+        while (true) {
+            input = Console.ReadLine();
+        }
+    }
+
+    public int handleInput() {
+        while (true) {
+            if (input != "null") {
+                
+                if (input == "exit") {
+                    return 1;
+                }
+                else if (input == "hi?") {
+                    Console.WriteLine("helloworld!");
+                }
+                input = "null";
+            }
+        }
+    }
+}
+
+class run {
+    static void Main()
+    {
+        Mayan test = new Mayan();
+
+        Thread tt = new Thread(test.getInput);
+        tt.Start();
+
+        if (test.handleInput() == 1)
+        {
+            tt.Abort();
+            return;
+            
+        }
+
+    }
+}
---
+++ Threading Test/Threading Test/Main.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+class Mayan
+{
+
+    static string input = "null";
+
+    private string temp;
+
+    public void getInput()
+    {
+        while (true)
+        {
+            input = Console.ReadLine();
+        }
+    }
+
+    public int handleInput()
+    {
+        while (true)
+        {
+            if (input != "null")
+            {
+                temp = input;
+                input = "null";
+                if (temp == "exit")
+                {
+                    return 1;
+                }
+                else if (temp == "hi?")
+                {
+                    Console.WriteLine("helloworld!");
+                }
+                else if (temp == "loopme") {
+                    for (int x = 0; x != 99; x++) {
+                        Console.Write("x");
+                    }
+                }
+                
+            }
+        }
+    }
+}
+
+class run
+{
+    static void Main()
+    {
+        Mayan test = new Mayan();
+
+        // this is running in a new thread called tt.
+        Thread tt = new Thread(test.getInput);
+        tt.Start();
+
+        // below is running in the main thread.
+        if (test.handleInput() == 1)
+        {
+            tt.Abort(); //if i don't abort the other thread it will keep running and console won't close even though I ran return
+            return;
+
+        }
+
+    }
+}
