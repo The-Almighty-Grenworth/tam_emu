08f21767d7a3ead37abfeb1049b05b035471600a
Josh <legril123@gmail.com>
2014-09-30 19:41:06 +1000

    Added class for handling input

---
+++ Tamagochi_Emulator/Tamagochi_Emulator/Parse_Input.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class InputHandler {
+    private static string input = "null";
+
+    public int beginParse() {
+        while (true) {
+
+
+
+
+
+            return 1;
+        }
+    }
+
+    public int listenForInput() { 
+        while (true) {
+        
+            return 1;
+        }
+    }
+
+    public string getInput() {
+        return input;
+    }
+    public void resetInput() {
+        input = "null";
+    }
+}
